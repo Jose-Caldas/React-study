c9606eaaf458cbacd1a3d3f32b979ff2d1d94c81
José Caldas <jrcaldas@yahoo.com.br>
2021-04-13 23:51:00 -0300

    install typeScript

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+interface RepositoryProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  };
+}
 
-function RepositoryItem(props) {
+function RepositoryItem(props: RepositoryProps) {
   return (
     <li>
       <strong>{props.repository.name}</strong>
       <p>{props.repository.description}</p>
+
       <a href={props.repository.html_url}>Acessar Repositório</a>
     </li>
   );
